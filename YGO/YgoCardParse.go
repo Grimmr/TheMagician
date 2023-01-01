455873a678697ffadfeb68f13433f9be10970518
grimmr <grimms767@gmail.com>
2023-01-01 20:51:39 +0000

    asorted fixes notibly stablity

--- YGO/YgoCardParse.go
+++ YGO/YgoCardParse.go
@@ -16,14 +16,19 @@ func getCardDataFromHtml(root *html.Node) map[string]string {
 	targets := []map[string]string{
 		{"title": "Password"},  //password title node
 		{"id": "firstHeading"}, //name
-		{"title": "Card type"}} //card type title node
+		{"title": "Card type"}, //card type title node
+		{"id": "section_0"}}    //name again for alt format pages
 
 	found := Html.FindNodesWithAttrs(root, targets)
 
 	pwNode := Html.FindNodesWithAttrs(found[0].Parent.Parent, []map[string]string{{"class": "mw-redirect"}})[0]
 	out["password"] = pwNode.FirstChild.Data
 
-	out["name"] = found[1].FirstChild.Data
+	if _, ok := found[1]; ok {
+		out["name"] = found[1].FirstChild.Data
+	} else {
+		out["name"] = found[3].FirstChild.Data
+	}
 
 	typeNode := Html.FindNodesWithAttrs(found[2].Parent.Parent, []map[string]string{{"class": ""}})[0]
 	out["type"] = typeNode.FirstChild.NextSibling.FirstChild.Data
